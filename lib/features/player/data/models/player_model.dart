edd118de92b55474dcb98c6de32c9f5370c89690
Shengael <luis@valdez.fr>
2020-06-16 00:34:44 +0200

    refacto

--- lib/features/player/data/models/player_model.dart
+++ lib/features/player/data/models/player_model.dart
@@ -3,15 +3,43 @@ import 'package:stat19_app_mobile/features/player/domain/entities/player.dart';
 
 class PlayerModel extends Player {
   PlayerModel(
-      {@required playerId, @required name, @required weight, @required height, @required birthday})
-      : super(playerId: playerId, name: name, weight: weight, height: height, birthday: birthday);
+      {@required playerId,
+      @required name,
+      @required weight,
+      @required height,
+      @required age,
+      @required playedMatches,
+      @required fouls,
+      @required redCards,
+      @required yellowCards,
+      @required shotOnTarget,
+      @required goals})
+      : super(
+            playerId: playerId,
+            name: name,
+            weight: weight,
+            height: height,
+            age: age,
+            playedMatches: playedMatches,
+            fouls: fouls,
+            redCards: redCards,
+            yellowCards: yellowCards,
+            shotOnTarget: shotOnTarget,
+            goals: goals);
 
   factory PlayerModel.fromJson(Map<String, dynamic> json) {
     return PlayerModel(
-        playerId: json['playerId'],
-        name: json['name'],
-        birthday: json['birthday'],
-        weight: json['weight'],
-        height: json['height']);
+      playerId: json['playerId'],
+      name: json['name'],
+      age: json['age'],
+      weight: json['weight'],
+      height: json['height'],
+      playedMatches: json['playedMatches'],
+      fouls: json['fouls'],
+      redCards: json['redCards'],
+      yellowCards: json['yellowCards'],
+      shotOnTarget: json['shotOnTarget'],
+      goals: json['goals'],
+    );
   }
 }
--- lib/features/player/domain/entities/player.dart
+++ lib/features/player/domain/entities/player.dart
@@ -6,17 +6,41 @@ class Player extends Equatable {
   final String name;
   final int weight;
   final int height;
-  final String birthday;
+  final String age;
+  final int playedMatches;
+  final int fouls;
+  final int redCards;
+  final int yellowCards;
+  final int shotOnTarget;
+  final int goals;
 
   Player(
       {@required this.playerId,
       @required this.name,
       @required this.weight,
-      @required this.birthday,
-      @required this.height});
+      @required this.age,
+      @required this.height,
+      @required this.playedMatches,
+      @required this.fouls,
+      @required this.redCards,
+      @required this.yellowCards,
+      @required this.shotOnTarget,
+      @required this.goals});
 
   @override
-  List<Object> get props => [playerId, name, weight, height, birthday];
+  List<Object> get props => [
+        playerId,
+        name,
+        weight,
+        height,
+        age,
+        playedMatches,
+        fouls,
+        redCards,
+        yellowCards,
+        shotOnTarget,
+        goals
+      ];
 
   @override
   bool get stringify => true;
