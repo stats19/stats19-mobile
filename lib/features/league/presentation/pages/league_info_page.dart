ad07fbcb806c59f17f7304251683ccb36cbb5b6f
Pierre Sid-idris <pierre.sididris@live.fr>
2020-06-26 01:36:33 +0200

    league info view fix token issue

--- lib/features/league/presentation/pages/league_info_page.dart
+++ lib/features/league/presentation/pages/league_info_page.dart
@@ -12,7 +12,8 @@ class LeagueInfoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: TitleLeague(leagueId: leagueId,)
+        title: Text("toto")
+//        title: TitleLeague(leagueId: leagueId,)
       ),
       body: buildBody(context),
     );
@@ -26,31 +27,40 @@ class LeagueInfoPage extends StatelessWidget {
       child: Center(
         child: Padding(
           padding: const EdgeInsets.all(10),
-//          child: Container(
-//              alignment: Alignment.topCenter,
-//              child: BlocBuilder<LeaguesBloc, LeaguesState>(
-//                  builder: (context, state) {
-//                    if ((state is Empty)) {
-//                      BlocProvider.of<LeaguesBloc>(context)
-//                          .add(GetMatchByLeagueEvent(leagueId));
-//                      print(state.props);
-//                      return Container();
-//                    } else if (state is Loading) {
-//                      print('loading');
-////                      return Center(child: CircularProgressIndicator());
-//                    } else if (state is MatchesByLeagueLoaded) {
-//                      print(state);
-////                      return Container(
-////                          margin: const EdgeInsets.all(10.0),
-////                          child: Text('data')
-////                      );
-//                    } else if (state is Error) {
-//                      print(state.message);
-//                      // return MessageDisplay(message: state.message);
-//                      return Text('there is error' + state.message);
-//                    }
-//                    return Container();
-//                  })),
+          child: Container(
+              alignment: Alignment.topCenter,
+              child: BlocBuilder<LeaguesBloc, LeaguesState>(
+                  builder: (context, state) {
+                    if ((state is Empty)) {
+                      BlocProvider.of<LeaguesBloc>(context)
+                          .add(GetMatchByLeagueEvent(leagueId));
+                      return Container();
+                    } else if (state is Loading) {
+                        return Center(child: CircularProgressIndicator());
+                    } else if (state is MatchesByLeagueLoaded) {
+                        print(state.matchesByLeague.toString());
+                        if(state.matchesByLeague is Empty){
+                          return Container(
+                            color: Colors.black,
+                            height: 100,
+                            width: 200,
+                            child: Text("pas de match à venir", style: TextStyle(color: Colors.red),),
+                          );
+                        }
+                        else{
+                          return Container(
+                            margin: const EdgeInsets.all(10.0),
+                            child: Text(state.matchesByLeague.toString())
+//                            child: Text("putin de sa race")
+                        );
+                        }
+//
+                    } else if (state is Error) {
+                      // return MessageDisplay(message: state.message);
+                      return Text('there is error' + state.message);
+                    }
+                    return Container();
+                  })),
         ),
       ),
     );
