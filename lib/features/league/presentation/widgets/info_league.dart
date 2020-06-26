204ae3f8e84ea01f9cdff2e6dbe8f1215a2d200d
Pierre Sid-idris <pierre.sididris@live.fr>
2020-06-26 03:33:46 +0200

    league page redirection

--- lib/features/league/presentation/widgets/info_league.dart
+++ lib/features/league/presentation/widgets/info_league.dart
@@ -46,8 +46,25 @@ class InfoLeague extends StatelessWidget {
                         child: TabBarView(
                             children: [
                               FuturMatch(leagueId: leagueId),
-                              Text("toto"),
-                            ]
+                              Container(
+                                color: Colors.white,
+                                padding: EdgeInsets.all(20.0),
+                                child: Table(
+                                  border: TableBorder.all(color: Colors.black),
+                                  children: [
+                                    TableRow(children: [
+                                      Text('Cell 1'),
+                                      Text('Cell 2'),
+                                      Text('Cell 3'),
+                                    ]),
+                                    TableRow(children: [
+                                      Text('Cell 4'),
+                                      Text('Cell 5'),
+                                      Text('Cell 6'),
+                                    ])
+                                  ],
+                                ),
+                              )                            ]
                         )
                     )
                   ]
