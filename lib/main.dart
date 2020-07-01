fbf9642afb00e40bb5c298b4c0252a49c0a93547
Shengael <luis@valdez.fr>
2020-07-01 11:06:22 +0200

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 import 'app.dart';
 import 'features/authentication/data/datasources/user_local_data_source.dart';
@@ -21,19 +22,41 @@ void main() async {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   final String token;
 
   const MyApp({Key key, this.token}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'router',
-      home: token != null ? App() : AuthenticationPage(),
-      // List all of the app's supported locales here
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
+      home: SplashScreen(
+          seconds: 3,
+          navigateAfterSeconds: MaterialApp(
+            title: 'router',
+            home: widget.token != null ? App() : AuthenticationPage(),
+            // List all of the app's supported locales here
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+          ),
+          title: new Text('Stats19',
+            style: new TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0
+            ),),
+          image: new Image.asset('lib/resources/assets/splash.png'),
+          backgroundColor: Colors.white,
+          styleTextUnderTheLoader: new TextStyle(),
+          photoSize: 100.0,
+          onClick: ()=>print("Flutter Egypt"),
+          loaderColor: Colors.red
+      ),
     );
   }
 }
